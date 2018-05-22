d40f1af5883648e31c6249f8384cf1068bdff215
Rakzodia <jeec.estaba@gmail.com>
2018-05-22 23:47:33 +0000

    [fix] syntax error geolocation

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -25,7 +25,7 @@ export class ContactPage {
 
 var getDistance = function(p1, p2) {
   var R = 6378137; // Earth’s mean radius in meter
-  var dLat = rad(p2.latitude - p1.latitude;
+  var dLat = rad(p2.latitude - p1.latitude);
   var dLong = rad(p2.longitude - p1.longitude);
   var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
