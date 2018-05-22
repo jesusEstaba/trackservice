a77c346460347e55225cea23821c0013a62a3ace
Rakzodia <jeec.estaba@gmail.com>
2018-05-22 22:55:24 +0000

    [add] activated message

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -8,12 +8,14 @@ import { GeolocationProvider } from '../../providers/geolocation/geolocation';
   providers: [GeolocationProvider]
 })
 export class ContactPage {
-  public latitude:any;
+  public latitude:any = 'fresh';
+  public activated:boolean = false;
   
   constructor(public navCtrl: NavController, public geo:GeolocationProvider) {}
   
   getGeo() {
     this.latitude = 'loading';
+    this.activated = true;
     
     this.geo.getGeolocation(coords => {
       this.latitude = coords.latitude
